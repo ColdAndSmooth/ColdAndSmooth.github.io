c4f77e6c9a69632f73b1f3d25224b06b107c0762
Jonathan Earles <earlesja@rose-hulman.edu>
2017-06-10 22:11:36 -0500

    Fixes

--- assets/js/local.js
+++ assets/js/local.js
@@ -1,6 +1,6 @@
 $.get("https://ipinfo.io", function(response) {
 	var subject = "Interest from - ";
-    var location = "/ City: " + response.city;
+    var location = "City: " + response.city;
     location += "/ Region: " + response.region;
     location += "/ Country: " + response.country;
     $('#subjectLine')[0].value = subject + location;
