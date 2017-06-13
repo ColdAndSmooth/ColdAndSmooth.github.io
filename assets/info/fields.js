eb6112da79d34f805fa8143e997344c950b059f5
Jonathan Earles <earlesja@rose-hulman.edu>
2017-06-12 23:20:39 -0500

    Polish

---
+++ assets/info/fields.js
@@ -0,0 +1,27 @@
+fields = {
+	navBarText: "NAV BAR TEXT",
+
+	topSectionText: "TOP TEXT",
+	topSubHeaderText: "TOP SUBHEADER TEXT",
+	topHeaderText: "TOP HEADER",
+
+	sectionOneText: "SOME TEXT",
+	sectionTwoText: "SOME TEXT TWO",
+	sectionThreeText: "SOME TEXT THREE",
+
+	contactUsHeader: "Show your interest! Contact Us:",
+
+	footerText: ""
+},
+
+headers = {
+
+	sectionOneHeader: "HEADER TEXT ONE",
+	sectionTwoHeader: "HEADER TEXT TWO",
+	sectionThreeHeader: "HEADER TEXT THREE"
+
+},
+
+images = {
+	backgroundImage: ""
+}
