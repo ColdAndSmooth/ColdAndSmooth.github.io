ecb21467eee62383aa1923745fe09653531cb617
Jonathan Earles <earlesja@rose-hulman.edu>
2017-06-18 17:47:37 -0500

    fonts

--- assets/info/fields.js
+++ assets/info/fields.js
@@ -1,9 +1,9 @@
 fields = {
 	navBarText: "Day4Nite",
 	
-	topHeaderText: "Day 4 Nite Supplements",
-	topSubHeaderText: "Enjoy your days so that you can enhance your night!",
-	topSectionText: "TOP TEXT",
+	topHeaderText: "Day 4 Nite<br>Supplements",
+	topSubHeaderText: "<b>enjoy your day. enhance your night!</b>",
+	topSectionText: "",
 
 	sectionOneText: "<b>Have you ever woken up after a night of dancing in a crowd of a thousand people, drained and unable to muster the energy to continue, only to realize, it's just day one. <i>That combination </i> of energy loss, sleeplessness and general hangover you feel is something we're all familiar with. By ingesting our specially <span style='color:red;'>formulated mix of anti-oxidants</span> and vitamins we can help fight the body drain you feel, as well as boosting your <li>immmune</li> <li style='text-align:right'>system and </li> limiting those day after effects.</b>",
 	sectionTwoText: "< DAYBREAK >",
