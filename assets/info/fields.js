85e9f3c728b32359c3c97a91699c9980b7023f8c
Jonathan Earles <earlesja@rose-hulman.edu>
2017-06-17 23:13:52 -0500

    A word

--- assets/info/fields.js
+++ assets/info/fields.js
@@ -5,7 +5,7 @@ fields = {
 	topSubHeaderText: "Enjoy your days so that you can enhance your night!",
 	topSectionText: "TOP TEXT",
 
-	sectionOneText: "Have you ever woken up after a night of dancing in a crowd of a thousand people, drained and unable to muster the energy to continue, only to realize, it's just day one. That combination of energy loss, sleeplessness and general hangover you feel is something we're all familiar with. By ingesting our intentionally formulated mix of anti-oxidants and vitamins we can help fight the body drain you feel, as well as boosting your immmune system and limiting those day after effects.",
+	sectionOneText: "Have you ever woken up after a night of dancing in a crowd of a thousand people, drained and unable to muster the energy to continue, only to realize, it's just day one. That combination of energy loss, sleeplessness and general hangover you feel is something we're all familiar with. By ingesting our specially formulated mix of anti-oxidants and vitamins we can help fight the body drain you feel, as well as boosting your immmune system and limiting those day after effects.",
 	sectionTwoText: "< DAYBREAK >",
 	sectionThreeText: "< LUCIDITY > ",
 	sectionFourText: "< AFTERGLOW > ",
