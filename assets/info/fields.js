ebfa329d378c1f687207ae35f69bc64045248b44
Jonathan Earles <earlesja@rose-hulman.edu>
2017-06-17 22:58:45 -0500

    words

--- assets/info/fields.js
+++ assets/info/fields.js
@@ -5,7 +5,7 @@ fields = {
 	topSubHeaderText: "Enjoy your days so that you can enhance your night!",
 	topSectionText: "TOP TEXT",
 
-	sectionOneText: "Have you ever woken up after a night of dancing in a crowd of a thousand people, drained and unable to muster the energy to continue just to realize, it's day one. That combination of energy loss, sleeplessness and general hangover you feel is caused by nerotoxins and cell damage caused by free radicals. By injesting specifically formulated mix of anti-oxidants and vitamins we can fight neurotoxity as well as boosting your immmune system and limiting those day after effects.",
+	sectionOneText: "Have you ever woken up after a night of dancing in a crowd of a thousand people, drained and unable to muster the energy to continue, only to realize, it's just day one. That combination of energy loss, sleeplessness and general hangover you feel is something we're all familiar with. By ingesting our intentionally formulated mix of anti-oxidants and vitamins we can help fight the body drain you feel, as well as boosting your immmune system and limiting those day after effects.",
 	sectionTwoText: "< DAYBREAK >",
 	sectionThreeText: "< LUCIDITY > ",
 	sectionFourText: "< AFTERGLOW > ",
