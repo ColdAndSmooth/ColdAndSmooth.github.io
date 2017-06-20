cd8af0a6c64919ab2f7a13f9ce8a89d6adc620b5
Jonathan Earles <earlesja@rose-hulman.edu>
2017-06-20 00:25:39 -0500

    Done

--- assets/info/fields.js
+++ assets/info/fields.js
@@ -9,8 +9,8 @@ fields = {
 
 	sectionTwoText: "<span style='text-align:left'><br>"+
 	"This supplement stack has been formulated to pre-load your body to minimize side effects from large doses of vitamins, while increasing the benefits of the other nutrients in LUCIDITY."+
-	"<li><b>Alpha Lipoic Acid (R-ALA):</b></li>"+
-	"<li><b>Acetyl-L-carnitine (ALCAR):</b></li> "+
+	"<li><b>Alpha Lipoic Acid (R-ALA):</b></li> Understood to be a powerful \"Universal antioxidant\" being both water and fat soluble will allow it to scavenge reactive oxygen and nitorgen species throughout the body. Comonly used to combat radiation sickenss, repair damaged livers and treat diabeties, R-ALA offers neuroprotectivity with an added benifit of regenerating other vitamins, like C, E, CoQ10, and Glutathione. This should be regaurded as a manditory supplement for everyone at a festival or duirng any strainous activities."+
+	"<li><b>Acetyl-L-Carnitine (ALCAR):</b></li> helps the body produce energy, along side synergistically improving the effectiveness of R-ALA and protecting against oxidative stress."+
 	"<li><b>Magnesium Glycinate:</b></li> High bioactive elemental Magnesium will enable your body to naturally protect itself from excitotoxicity clearing the ion channels necessary for cascading electrical sigals in your nerons. Magnesium Glycinate is one of many chelated compounds enabling high bioavailabity and absorption rate. A small amount of people may experince upset GI-trac with large servings of elemental Magnesium to reduce this, take DAYBREAK in the morning before LUCIDITY to prep the body later. "+
 	"<li><b>Vitamin C:</b></li> A very common over the counter anti-oxidant found in many fruits believed to boost the immmune system also used to protect against neurotoxitity from depleted serotonin levels. Vitamin C will also raise the stomach acidity which will help excrete many harmful substances through urination"+
 	"Directions: Mix one powder pack with 16+ oz of water and drink with breakfast during the festival."+
@@ -18,10 +18,10 @@ fields = {
 	
 	sectionThreeText: "<span style='text-align:left'><br>"+
 	"This supplement stack has been specially formulated to maximize your brains neurotoxic defence. We have developed a booster stack of vitamins that continue to neurotoxic defense and "+
-	"<li><b>Alpha Lipoic Acid (R-ALA):</b></li>"+
-	"<li><b>Acetyl-L-carnitine (ALCAR):</b></li> "+
-	"<li><b>Coenzyme (CoQ10):</b></li>  "+
-	"<li><b>Grape seed Extract:</b></li>"+
+	"<li><b>Alpha Lipoic Acid (R-ALA):</b></li> Understood to be a powerful \"Universal antioxidant\" being both water and fat soluble will allow it to scavenge reactive oxygen and nitorgen species throughout the body. Comonly used to combat radiation sickenss, repair damaged livers and treat diabeties, R-ALA offers neuroprotectivity with an added benifit of regenerating other vitamins, like C, E, CoQ10, and Glutathione. This should be regaurded as a manditory supplement for everyone at a festival or duirng any strainous activities."+
+	"<li><b>Acetyl-L-carnitine (ALCAR):</b></li> helps the body produce energy, along side synergistically improving the effectiveness of R-ALA and protecting against oxidative stress."+
+	"<li><b>Coenzyme (CoQ10):</b></li> used to protect your neurons from excitotoxicity by synthesizing ATP and allowing your protein pumps to expel excess Ca2+, further helping to prevent damage to your nervous system."+
+	"<li><b>Grape seed Extract:</b></li> a diuretic that helps manage urinary retention assisting in flushing toxins as well as being a potent antioxidant."+
 	"<li><b>Magnesium Glycinate:</b></li> High bioactive elemental Magnesium will enable your body to naturally protect itself from excitotoxicity clearing the ion channels necessary for cascading electrical sigals in your nerons. Magnesium Glycinate is one of many chelated compounds enabling high bioavailabity and absorption rate. A small amount of people may experince upset GI-trac with large servings of elemental Magnesium to reduce this, take DAYBREAK in the morning before LUCIDITY to prep the body later. "+
 	"<li><b>Vitamin C:</b></li> A very common over the counter anti-oxidant found in many fruits believed to boost the immmune system also used to protect against neurotoxitity from depleted serotonin levels. Vitamin C will also raise the stomach acidity which will help excrete many harmful substances through urination"+
 	"<li><b>Vitamin B3 (Nicotinamide):</b></li>"+
@@ -36,7 +36,7 @@ fields = {
 	"<li><b>Panax Gensing:</b></li> "+
 	"Servings per container: five, Directions: Take one capsule per night before bed for one week 24 hours after strenuous event."+
 	"<span>",
-	sectionFiveText: "SOME TEXT THREE",
+	sectionFiveText: "",
 
 	contactUsHeader: "Show your interest! Contact Us:",
 
