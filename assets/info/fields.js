14eebb556eed641669d6cfa6aa09b9afeacb35c8
Jonathan Earles <earlesja@rose-hulman.edu>
2017-06-19 23:12:16 -0500

    Lawrence is dope at writing stuff

--- assets/info/fields.js
+++ assets/info/fields.js
@@ -6,9 +6,36 @@ fields = {
 	topSectionText: "",
 
 	sectionOneText: "<b>Have you ever woken up after a night of dancing in a crowd of a thousand people, drained and unable to muster the energy to continue, only to realize, it's just day one. <i>That combination </i> of energy loss, sleeplessness and general hangover you feel is something we're all familiar with. By ingesting our specially <span style='color:red;'>formulated mix of anti-oxidants</span> and vitamins we can help fight the body drain you feel, as well as boosting your <li>immmune</li> <li style='text-align:right'>system and </li> limiting those day after effects.</b>",
-	sectionTwoText: "< DAYBREAK >",
-	sectionThreeText: "< LUCIDITY > ",
-	sectionFourText: "< AFTERGLOW > ",
+
+	sectionTwoText: "<span style='text-align:left'><br>"+
+	"This supplement stack has been formulated to pre-load your body to minimize side effects from large doses of vitamins, while increasing the benefits of the other nutrients in LUCIDITY."+
+	"<li><b>Alpha Lipoic Acid (R-ALA):</b></li>"+
+	"<li><b>Acetyl-L-carnitine (ALCAR):</b></li> "+
+	"<li><b>Magnesium Glycinate:</b></li> High bioactive elemental Magnesium will enable your body to naturally protect itself from excitotoxicity clearing the ion channels necessary for cascading electrical sigals in your nerons. Magnesium Glycinate is one of many chelated compounds enabling high bioavailabity and absorption rate. A small amount of people may experince upset GI-trac with large servings of elemental Magnesium to reduce this, take DAYBREAK in the morning before LUCIDITY to prep the body later. "+
+	"<li><b>Vitamin C:</b></li> A very common over the counter anti-oxidant found in many fruits believed to boost the immmune system also used to protect against neurotoxitity from depleted serotonin levels. Vitamin C will also raise the stomach acidity which will help excrete many harmful substances through urination"+
+	"Directions: Mix one powder pack with 16+ oz of water and drink with breakfast during the festival."+
+	"<span>",
+	
+	sectionThreeText: "<span style='text-align:left'><br>"+
+	"This supplement stack has been specially formulated to maximize your brains neurotoxic defence. We have developed a "+
+	"<li><b>Alpha Lipoic Acid (R-ALA):</b></li>"+
+	"<li><b>Acetyl-L-carnitine (ALCAR):</b></li> "+
+	"<li><b>Coenzyme (CoQ10):</b></li>  "+
+	"<li><b>Grape seed Extract:</b></li>"+
+	"<li><b>Magnesium Glycinate:</b></li> High bioactive elemental Magnesium will enable your body to naturally protect itself from excitotoxicity clearing the ion channels necessary for cascading electrical sigals in your nerons. Magnesium Glycinate is one of many chelated compounds enabling high bioavailabity and absorption rate. A small amount of people may experince upset GI-trac with large servings of elemental Magnesium to reduce this, take DAYBREAK in the morning before LUCIDITY to prep the body later. "+
+	"<li><b>Vitamin C:</b></li> A very common over the counter anti-oxidant found in many fruits believed to boost the immmune system also used to protect against neurotoxitity from depleted serotonin levels. Vitamin C will also raise the stomach acidity which will help excrete many harmful substances through urination"+
+	"<li><b>Vitamin B3 (Nicotinamide):</b></li>"+
+	"Directions: Mix one powder pack with 16+ oz of water and drink during  per night before bed for one week 24 hours after strenuous event."+
+	"<span>",
+
+	sectionFourText: "<span style='text-align:left'><br>"+
+	"We sought out to create an after festival experience that enables you to go back to work monday with a reduced hangover and a good nights sleep.<br>"+
+	"<li><b>5-HTP:</b></li> A direct precursor to serotonin (5-HT) that has the ability to pass through the blood brain barrier and help aid in restoring serotonin levels. When taken at night, it results in waking up refreshed and reducing hangovers. "+
+	"<li><b>EGCG:</b></li> Inhibit DOPA decarboxylase which prevents 5-HTP from being broken down in the stomach or periphery, ensuring your brain is getting the 5-HTP and converting it to serotonin. 5-HTP should not be taken without EGCG, excess 5-HT in the periphery can cause heart valve damage."+
+	"<li><b>Melatonin:</b></li> A very common over the counter sleeping aid that is naturally produced from serotonin, moments before going to sleep. This will help start the sleeping process in the restless nights after."+
+	"<li><b>Panax Gensing:</b></li> "+
+	"Servings per container: five, Directions: Take one capsule per night before bed for one week 24 hours after strenuous event."+
+	"<span>",
 	sectionFiveText: "SOME TEXT THREE",
 
 	contactUsHeader: "Show your interest! Contact Us:",
@@ -19,9 +46,9 @@ fields = {
 headers = {
 
 	sectionOneHeader: "Company Info",
-	sectionTwoHeader: "DayBreak",
-	sectionThreeHeader: "Lucidity",
-	sectionFourHeader: "AfterGlow",
+	sectionTwoHeader: "DAYBREAK",
+	sectionThreeHeader: "LUCIDITY",
+	sectionFourHeader: "AFTERGLOW",
 	sectionFiveHeader: "Why, though?"
 
 },
